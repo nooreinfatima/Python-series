86076fe306c1364f80063290ab4e87137370b086
nooreinfatima <nooreinfatima43@gmail.com>
2026-03-08 19:13:26 +0000

    Data Types

---
+++ 01-PythonBasics/02-DataTypes.py
@@ -0,0 +1,26 @@
+##integer example
+age=35
+print(type(age))
+
+#Floating point datatypes
+height=5.11
+print(height)
+print(type(height))
+
+#Strings datatype example
+name="Noorein"
+print(name)
+print(type(name))
+
+#boolen data type
+
+is_true=True
+print(type(is_true))
+
+a=10
+b=10
+print(type(a==b))
+
+#typecasting
+result="Hello"+str(5)
+print(result)
