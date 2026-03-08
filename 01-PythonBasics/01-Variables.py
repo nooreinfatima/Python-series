b64998854b4e1476b93bb9d5d5c64a89722d434e
nooreinfatima <nooreinfatima43@gmail.com>
2026-03-08 18:26:13 +0000

    Variables

---
+++ 01-PythonBasics/01-Variables.py
@@ -0,0 +1,18 @@
+#print("Hello World")
+
+#Declearing and assigning variables
+
+'''age=32
+height=6.1
+name="Noorein"
+is_student=True
+
+print(type(age))
+print("Age:",age)
+print(type(height))
+print("Height:",height)
+print(type(name))
+print("Name:",name)
+print(type(is_student))
+print("Student:",is_student)'''
+
