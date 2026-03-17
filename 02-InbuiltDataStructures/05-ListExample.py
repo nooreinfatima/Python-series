6ba3a5dbdda86554c650c0836dff74b06cac50f1
nooreinfatima <nooreinfatima43@gmail.com>
2026-03-17 19:05:06 +0000

    Example-2

--- 02-InbuiltDataStructures/05-ListExample.py
+++ 02-InbuiltDataStructures/05-ListExample.py
@@ -19,3 +19,22 @@ for task in to_do_list:
   print(f"-{task}")
   
   
+## Example 2
+
+grades=[85,92,78,90,88]
+
+## Adding a new grades
+grades.append(95)
+print(grades)
+
+#Calculating the average grades
+average_grade = sum(grades) / len(grades)
+print(f"Average Grade: {average_grade:.2f}")
+
+# Finding the highest and lowest grades
+
+highest_grade = max(grades)
+lowest_grade = min(grades)
+
+print(f"Highest Grade: {highest_grade}")
+print(f"Lowest Grade: {lowest_grade}")
